7cdd64bacc16974f8c1daa69dfe70d9faaaf530f
runscible <runscible@gmail.com>
2017-12-09 22:45:38 -0300

    las commit

--- app/Http/Controllers/soupController.php
+++ app/Http/Controllers/soupController.php
@@ -49,75 +49,67 @@ function get_results_soup($len_char, $len_row){
            for($horizontal = 0 ;$horizontal < count($results_soup[$vertical]); $horizontal ++ ): 
              //encuentra valores horizontales
               if($horizontal < count($results_soup[$vertical])-4 &&
-                   $results_soup[$vertical][$horizontal]   == 'E' &&
+                   $results_soup[$vertical][$horizontal]   == 'O' &&
                    $results_soup[$vertical][$horizontal+1] == 'I' && 
-                   $results_soup[$vertical][$horizontal+2] == 'O' && 
-                   $results_soup[$vertical][$horizontal+3] == 'U'):
+                   $results_soup[$vertical][$horizontal+2] == 'E'):
                    $counter_horizontal_results ++;               
                endif;
                //resultado horizontal invertido 
                if($horizontal > 4 &&
-                   $results_soup[$vertical][$horizontal]   == 'E' && 
+                   $results_soup[$vertical][$horizontal]   == 'O' && 
                    $results_soup[$vertical][$horizontal-1] == 'I' && 
-                   $results_soup[$vertical][$horizontal-2] == 'O' && 
-                   $results_soup[$vertical][$horizontal-3] == 'U'): 
+                   $results_soup[$vertical][$horizontal-2] == 'E'): 
                    $counter_horizontal_results_invert ++; 
                endif;
                //resultado vertical 
                if( $vertical < $vertical-4 &&
                $results_soup[$vertical+1][$horizontal] == 'I' &&
-                   $results_soup[$vertical][$horizontal]   == 'E' && 
-                   $results_soup[$vertical+2][$horizontal] == 'O' &&
-                   $results_soup[$vertical+3][$horizontal] == 'U'):
+                   $results_soup[$vertical][$horizontal]   == 'O' && 
+                   $results_soup[$vertical+2][$horizontal] == 'I' &&
+                   $results_soup[$vertical+3][$horizontal] == 'E'):
                    $counter_vertical_results ++; 
                endif;
                
                //resultado vertical invertido 
                if( $vertical > 3 &&
-                   $results_soup[$vertical][$horizontal]   == 'E' &&   
+                   $results_soup[$vertical][$horizontal]   == 'O' &&   
                    $results_soup[$vertical-1][$horizontal] == 'I' &&
-                   $results_soup[$vertical-2][$horizontal] == 'O' &&
-                   $results_soup[$vertical-3][$horizontal] == 'U'
-                   ):  
+                   $results_soup[$vertical-2][$horizontal] == 'E'):  
                    $counter_horizontal_results_invert ++; 
                endif; 
 
                //resultado diagonal superior derecha 
                if($horizontal < $horizontal-4 &&
                    $vertical < $vertical -4 && 
-                   $results_soup[$vertical][$horizontal]       == 'E'&&
+                   $results_soup[$vertical][$horizontal]       == 'O'&&
                    $results_soup[$vertical +1][$horizontal -1] == 'I' &&
-                   $results_soup[$vertical +2][$horizontal -2] == 'O' &&
-                   $results_soup[$vertical +3][$horizontal -3] == 'U'): 
+                   $results_soup[$vertical +2][$horizontal -2] == 'E'): 
                   $counter_diagonal_up_right ++; 
                endif;     
 
                //resultado diagonal superior izquierda
                if($horizontal > 3 && 
                    $vertical  > 3 &&
-                   $results_soup[$vertical][$horizontal]    == 'E'&&
+                   $results_soup[$vertical][$horizontal]    == 'O'&&
                    $results_soup[$vertical-1][$horizontal-1]== 'I'&&
-                   $results_soup[$vertical-2][$horizontal-2]== 'O'&&
-                   $results_soup[$vertical-3][$horizontal-3]== 'U'): 
+                   $results_soup[$vertical-2][$horizontal-2]== 'E'): 
                    $counter_diagonal_up_left ++; 
                endif;      
              
                //resultado diagonal inferior derecho 
               if($horizontal < $horizontal-4 && 
                   $vertical < $vertical -4 &&     
-                   $results_soup[$vertical][$horizontal]      == 'E'&&
+                   $results_soup[$vertical][$horizontal]      == 'O'&&
                    $results_soup[$vertical-1][$horizontal+1]  == 'I'&&
-                   $results_soup[$vertical-2][$horizontal+2]  == 'O'&&
-                   $results_soup[$vertical-3][$horizontal+3]  == 'U'):
+                   $results_soup[$vertical-2][$horizontal+2]  == 'E'):
                    $counter_diagonal_down_right ++; 
               endif;   
            //resultado diagonal inferior izquierdo 
            if($horizontal > 3 && 
               $vertical > 3 &&
-               $results_soup[$vertical][$horizontal]    == 'E'&&
+               $results_soup[$vertical][$horizontal]      == 'O'&&
                $results_soup[$vertical-1][$horizontal-1]  == 'I'&&
-               $results_soup[$vertical-2][$horizontal-2]  == 'O'&&
-               $results_soup[$vertical-3][$horizontal-3]  == 'U'):
+               $results_soup[$vertical-2][$horizontal-2]  == 'E'):
                $counter_diagonal_down_left ++; 
            endif;
        endfor;      
--- readme.md
+++ readme.md
@@ -1,3 +1,27 @@
 ###
 larasoup
 ###
+funcionamiento de la aplicación :
+//del lado del front: 
+ 
+la aplicacion consiste en generar una sopa de letras a partir de 
+dos parametros que llegan por el lado de la vista, uno es el largo de 
+las filas de las que consta la sopa , y en segunda instancia la cantidad 
+de filas generadas 
+
+//del lado del back
+
+el controlador de la aplicación consta de tres funciones anidadas que 
+se retroalimentan entre ellas y permite a la lógica de la aplicaión 
+permanecer separada en partes, ellas son: 
+
+*char_ran : genera un array de letras aleatorias a partir de un 
+parámetro dado desde el front 
+
+*get_soup : instancia a char_ran dentro de un de un for que va a iterar 
+una cantidad de veces establecida 'X' veces desde el lado del front 
+
+*get_results_soup: instancia a get_soup y toma el array ya generado, a 
+partir de este itera buscando el primer caracter de la palabra buscada y 
+consiguiente dependiendo del orden en que se necesite encontrar (sea 
+vertical , horizonta, invertida, diagonal , etc ) 
